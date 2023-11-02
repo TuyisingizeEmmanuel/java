26d5e51beacda71e53c22718a90ab722289d378b
TuyisingizeEmmanuel <141547023+TuyisingizeEmmanuel@users.noreply.github.com>
2023-11-02 08:58:52 +0200

    Update ForExample.java
    this for loop example

--- ForExample.java
+++ ForExample.java
@@ -1,16 +1,11 @@
 public class ForExample {// declaration of the class and class name
-	public static void main(String args[]) {/*
-											 * t is a keyword and is used to specify that a method doesn't return
-											 * anything. As the main() method doesn't return anything, its return type
-											 * is void. As soon as the main() method terminates, the java program
-											 * terminates too.
+	public static void main(String args[]) {/*this is a keyword and is used to specify that a method doesn't return anything. As the main() method doesn't return anything, its return type is void. As soon as the main() method terminates, the java program
+			terminates too.
 											 */
 		for (int j = 1; j <= 10; j++) {// this line shwos initialization,condotion and incrementation
 
-			System.out.println("HELLO MY PEOPLE");/*
-													 * the code below will print the words Hello my people
-													 * to the screen, and it is amazing
-													 */
+			System.out.println("HELLO MY PEOPLE");/* the code below will print the words Hello my people to the screen, and it is amazing
+													*/
 		}
 	}
 
